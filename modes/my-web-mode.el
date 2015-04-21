77d34845cabf54b7a186199b9287b8f0b568cc6a
Francisco Soto <bobby@hellocode.mx>
2015-04-20 18:33:33 -0700

    Revamp our webdev environment.

--- modes/my-web-mode.el
+++ modes/my-web-mode.el
@@ -4,20 +4,31 @@
 
 ;;; Code:
 
-(require-packages '(web-mode sass-mode rainbow-mode))
+(require-packages '(web-mode sass-mode rainbow-mode jsx-mode react-snippets coffee-mode))
 
 (require 'web-mode)
 (require 'rainbow-mode)
+(require 'jsx-mode)
+(require 'coffee-mode)
 
+;; Rainbow mode
 (add-hook 'css-mode-hook (lambda () (rainbow-mode 1)))
 (add-hook 'html-mode-hook (lambda () (rainbow-mode 1)))
 (add-hook 'sass-mode-hook (lambda () (rainbow-mode 1)))
 (diminish 'rainbow-mode)
 
+;; Web mode
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 4)
 
+;; JSX mode
+(setq jsx-indent-level 2)
+
+;; Coffeescript
+(custom-set-variables '(coffee-tab-width 2))
+
+;; What to load with what
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
@@ -26,6 +37,7 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
 
 (provide 'my-web-mode)
 
