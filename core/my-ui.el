ca60df6733c025ffba65b8faccc3c2faf69669fc
Francisco Soto <bobby@hellocode.mx>
2015-05-09 15:22:51 -0700

    Fix comment.

--- core/my-ui.el
+++ core/my-ui.el
@@ -44,7 +44,7 @@
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
-; Turn off mouse interface early in startup to avoid momentary display
+;; Turn off mouse interface early in startup to avoid momentary display
 ;; You really don't need these; trust me.
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
