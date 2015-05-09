13360c695f4ec4316080ccf6f9bbcd83a0378104
Francisco Soto <bobby@hellocode.mx>
2015-05-09 16:46:33 -0700

    Some utilities.

--- core/my-utilities.el
+++ core/my-utilities.el
@@ -10,6 +10,21 @@
   (epl-refresh)
   (epl-upgrade))
 
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (indent-buffer)
+  (untabify-buffer)
+  (delete-trailing-whitespace))
+
 (provide 'my-utilities)
 
 ;;; my-utilities.el ends here
