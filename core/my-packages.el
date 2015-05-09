ee86cc34d28aee59e8c1558e884005e29f1f3db9
Francisco Soto <bobby@hellocode.mx>
2015-05-09 15:18:16 -0700

    Correct host for melpa.

--- core/my-packages.el
+++ core/my-packages.el
@@ -7,7 +7,7 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
 (defvar my-packages '(ace-jump-buffer
