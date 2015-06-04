fa8a6d3d0cdfd65f7e30207624b272d2b219ede0
Francisco Soto <ebobby@gmail.com>
2015-06-04 00:02:17 -0700

    Actual 50MB.

--- init.el
+++ init.el
@@ -34,7 +34,7 @@
 
 ;; reduce the frequency of garbage collection by making it happen on
 ;; each 50MB of allocated data (the default is on every 0.76MB)
-(setq gc-cons-threshold 50000000)
+(setq gc-cons-threshold (* 1024 1024 50))
 
 ;; warn when opening files bigger than 100MB
 (setq large-file-warning-threshold 100000000)
