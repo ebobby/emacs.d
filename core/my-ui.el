f8d9c45f2ab73c30525e92ba7993565e62ed3b43
Francisco Soto <ebobby@ebobby.org>
2015-07-02 15:18:33 -0700

    Theme change.

--- core/my-ui.el
+++ core/my-ui.el
@@ -61,9 +61,9 @@
 (setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
 (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
 (setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
-(moe-theme-set-color 'orange)
+(moe-theme-set-color 'magenta)
 
-(moe-light)
+(moe-dark)
 (powerline-moe-theme)
 
 (setq powerline-display-buffer-size nil)
