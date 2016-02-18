5c2190e6a97955467a5d6059c8e1e8fd7c97866e
Francisco Soto <ebobby@ebobby.org>
2016-02-18 10:11:59 -0700

    Use js2 ide for JSX instead of web mode.

--- modules/my-js.el
+++ modules/my-js.el
@@ -10,6 +10,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.js\\'"    . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.pac\\'"   . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'"   . js2-jsx-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 (setq-default js2-basic-offset 2)
@@ -23,6 +24,7 @@
     (add-to-list 'company-backends 'company-tern)))
 
 (add-hook 'js2-mode-hook 'setup-js2)
+(add-hook 'js2-jsx-mode-hook 'setup-js2)
 
 (provide 'my-js)
 
--- modules/my-web.el
+++ modules/my-web.el
@@ -47,7 +47,6 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
