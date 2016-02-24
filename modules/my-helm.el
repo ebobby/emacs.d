64aff0de90e297a257cafa522ee508b9307527d2
Francisco Soto <ebobby@ebobby.org>
2016-02-24 15:21:12 -0700

    Change helm hotkeys.

--- modules/my-helm.el
+++ modules/my-helm.el
@@ -57,6 +57,10 @@
 ;; shell history.
 (define-key shell-mode-map (kbd "C-c C-l") 'helm-comint-input-ring)
 
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z") 'helm-select-action) ; list actions using C-z
+
 (substitute-key-definition 'find-tag 'helm-etags-select global-map)
 (setq projectile-completion-system 'helm)
 (helm-descbinds-mode)
