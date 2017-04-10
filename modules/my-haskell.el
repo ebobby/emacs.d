670a7047f753facb58b38b959257bc9ef0ae5e70
Francisco Soto <ebobby@ebobby.org>
2017-04-10 10:41:14 -0700

    Haskell mode fixup.

--- modules/my-haskell.el
+++ modules/my-haskell.el
@@ -4,13 +4,17 @@
 
 ;;; Code:
 
-(require-packages '(haskell-mode))
+(require-packages '(haskell-mode intero flycheck-haskell))
 
 ;; Haskell
 (require 'haskell-mode)
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'intero-mode)
+
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
 
 (provide 'my-haskell)
 
