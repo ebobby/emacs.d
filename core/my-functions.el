800f15a5c8e18680e24293d3b417a130a9fb391e
Francisco Soto <ebobby@ebobby.org>
2018-07-01 14:22:47 -0700

    Rename transparency function.

--- core/my-functions.el
+++ core/my-functions.el
@@ -22,7 +22,7 @@
   (untabify (point-min) (point-max)))
 
 ;; Set transparency of emacs
-(defun transparency (value)
+(defun my-transparency (value)
   "Sets the transparency of the frame window. 0=transparent/100=opaque"
   (interactive "nTransparency Value 0 - 100 opaque: ")
   (set-frame-parameter (selected-frame) 'alpha value))
