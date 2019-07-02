e8f15b4d8572eff3f75bc13b02ecbf06e666f31b
Francisco Soto <ebobby@ebobby.org>
2019-07-02 12:07:17 -0700

    Fix shell path.

--- core/my-editor.el
+++ core/my-editor.el
@@ -206,7 +206,7 @@
 
 ;; Get path from shell
 (require 'exec-path-from-shell)
-(setq exec-path-from-shell-arguments '("-l"))
+(setq exec-path-from-shell-arguments '("-i"))
 (exec-path-from-shell-initialize)
 
 ;; Which key
