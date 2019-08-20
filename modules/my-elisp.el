c1b6a7d6323d90a693e611d05a3f9ec1464cb68b
Francisco Soto <ebobby@ebobby.org>
2019-08-20 08:30:31 -0700

    Remove unused modules.

--- modules/my-elisp.el
+++ modules/my-elisp.el
@@ -4,20 +4,15 @@
 
 ;;; Code:
 
-(require-packages '(elisp-slime-nav))
-
 (define-key emacs-lisp-mode-map (kbd "C-c C-c") 'eval-defun)
 (define-key emacs-lisp-mode-map (kbd "C-c C-b") 'eval-buffer)
 
 (add-hook 'emacs-lisp-mode-hook (lambda ()
                                    (eldoc-mode +1)
                                    (rainbow-mode +1)
-                                   (elisp-slime-nav-mode +1)
                                    (smartparens-strict-mode +1)
                                    (setq mode-name "ELisp")))
 
-(eval-after-load "elisp-slime-nav"
-  '(diminish 'elisp-slime-nav-mode))
 (eval-after-load "eldoc"
   '(diminish 'eldoc-mode))
 
