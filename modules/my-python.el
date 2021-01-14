d6ebbbfc2cc625da00781586385a0be62bfea604
Francisco Soto <ebobby@ebobby.org>
2021-01-13 19:54:33 -0700

    Python virtual envs and repl.

--- modules/my-python.el
+++ modules/my-python.el
@@ -4,22 +4,41 @@
 
 ;;; Code:
 
-(require-packages '(pyenv-mode lsp-python-ms py-autopep8))
+(require-packages '(pyvenv pyenv-mode lsp-python-ms py-autopep8))
+
+(defun run-python-for-project ()
+  "Run python process in the root of the project."
+  (interactive)
+  (let ((default-directory (or (helm-ls-git-root-dir)
+                               default-directory)))
+    (run-python)))
+
+(defun setup-python-virtualenv ()
+  "If a virtualenv is available, use it."
+  (let* ((project-dir (helm-ls-git-root-dir))
+         (venv-dir (or (expand-file-name "venv" project-dir)
+                       (expand-file-name "env" project-dir))))
+    (if (file-directory-p venv-dir)
+        (pyvenv-activate venv-dir))))
 
 ;; Enable pyenv mode first
 (pyenv-mode)
 
 (add-hook 'python-mode-hook (lambda ()
-                              (py-autopep8-enable-on-save)
                               (require 'lsp-python-ms)
+                              (setup-python-virtualenv)
+                              (py-autopep8-enable-on-save)
                               (lsp)))
 
 ;; Auto install Microsoft's Python Server
 (setq lsp-python-ms-auto-install-server t)
 
 ;; Use IPython
-(setq python-shell-interpreter "ipython"
+(setq python-shell-interpreter      "ipython"
       python-shell-interpreter-args "-i --simple-prompt")
+
+;; Run python repl at the root of the project if possible.
+(define-key python-mode-map (kbd "C-c C-p") 'run-python-for-project)
 
 (provide 'my-python)
 
