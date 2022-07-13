d012efc0f5a7a4cb9750292a57491479b75dce5e
Francisco Soto <ebobby@ebobby.org>
2022-07-13 14:40:47 -0700

    Theme fixes.

--- core/my-theme.el
+++ core/my-theme.el
@@ -8,13 +8,13 @@
 ;;; Commentary:
 ;;; Code:
 
-(defvar ui-font "Consolas-13")
+(defvar ui-font "Fantasque Sans Mono-13")
 
 ;; All the icons!
 (use-package all-the-icons
   :config
   ;;(all-the-icons-install-fonts t)
-  (setq all-the-icons-scale-factor 1))
+  (setq all-the-icons-scale-factor 1.2))
 
 ;; Doom theme.
 (use-package doom-themes
@@ -25,7 +25,7 @@
         doom-molokai-brighter-comments t
         doom-molokai-brighter-modeline t)
 
-  (load-theme 'doom-molokai t)
+  (load-theme 'doom-dracula t)
 
   (doom-themes-visual-bell-config)
   (doom-themes-neotree-config)
@@ -35,15 +35,35 @@
   :after (doom-themes)
   :hook (after-init . doom-modeline-mode)
   :config
-  ;;(setq doom-modeline-height 50)
   (setq doom-modeline-icon t)
+  (setq doom-modeline-height 25)
   (setq doom-modeline-project-detection 'projectile)
-  (setq doom-modeline-buffer-file-name-style 'truncate-with-project))
+  (setq doom-modeline-buffer-file-name-style 'truncate-with-project)
+  (setq doom-modeline-bar-width 1)
+  (setq doom-modeline-major-mode-icon t)
+  (setq doom-modeline-major-mode-color-icon t)
+  (setq doom-modeline-buffer-state-icon t)
+  (setq doom-modeline-buffer-modification-icon t)
+  (setq doom-modeline-minor-modes nil)
+  (setq doom-modeline-enable-word-count nil)
+  (setq doom-modeline-buffer-encoding t)
+  (setq doom-modeline-indent-info nil)
+  (setq doom-modeline-checker-simple-format t)
+  (setq doom-modeline-vcs-max-length 12)
+  (setq doom-modeline-env-version t)
+  (setq doom-modeline-irc-stylize 'identity)
+  (setq doom-modeline-github-timer nil)
+  (setq doom-modeline-gnus-timer nil)
+
+  (with-eval-after-load "doom-modeline"
+    (doom-modeline-def-modeline 'main
+      '(bar workspace-name window-number modals matches follow buffer-info remote-host buffer-position word-count parrot selection-info)
+      '(objed-state misc-info persp-name battery grip irc mu4e gnus github debug repl lsp minor-modes input-method indent-info buffer-encoding major-mode process vcs checker "   "))))
 
 ;; Frame and font setup for standalone emacs.
 (when window-system
   (set-frame-parameter nil 'fullscreen 'maximized)
-  (set-face-attribute 'default nil :font ui-font))
+  (set-frame-font ui-font))
 
 (provide 'my-theme)
 
