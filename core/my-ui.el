3abbd355b08abe1e01a6803452a3d39c9e3b2206
Francisco Soto <ebobby@ebobby.org>
2023-06-10 14:37:56 -0700

    Dracula.

--- core/my-ui.el
+++ core/my-ui.el
@@ -15,11 +15,11 @@
         doom-themes-enable-italic t
         doom-themes-neotree-file-icons t)
 
-  (load-theme 'doom-monokai-pro t)
+  (load-theme 'doom-dracula t)
 
   ;; doom-monokai-pro helm-files directories are white for some reason.
-  (custom-set-faces
-   '(helm-ff-directory ((t (:extend t :foreground "#FFD866")))))
+;;  (custom-set-faces
+;;   '(helm-ff-directory ((t (:extend t :foreground "#FFD866")))))
 
   (doom-themes-visual-bell-config)
   (doom-themes-neotree-config)
