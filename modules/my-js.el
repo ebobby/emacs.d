f02cbe01f09cd875ae710f70ee93c5df709aa288
Francisco Soto <ebobby@ebobby.org>
2023-09-07 23:10:30 -0700

    Use tree-sitter Javascript mode.

--- modules/my-js.el
+++ modules/my-js.el
@@ -13,30 +13,20 @@
 (use-package json-ts-mode
   :mode (("\\.json\\'" . json-ts-mode)))
 
-(use-package js2-mode
-  :hook (((js2-mode js2-mode-jsx) . js2-imenu-extras-mode)
-         (js2-mode . lsp)
-         (js2-mode . dap-mode))
+(use-package js-ts-mode
+  :hook ((js-ts-mode . lsp)
+         (js-ts-mode . dap-mode))
+  :mode (("\\.js\\'"  . js-ts-mode)
+         ("\\.jsx\\'" . tsx-ts-mode))
   :interpreter "node"
   :config
   (require 'dap-node)
   (require 'dap-firefox)
   (setq dap-firefox-debug-program `("node" ,(expand-file-name "extension/dist/adapter.bundle.js"
-                                                              dap-firefox-debug-path))
-        js-chain-indent t
-        js2-basic-offset 2
-        js2-highlight-external-variables t
-        js2-highlight-level 3
-        js2-idle-timer-delay 0.1
-        js2-mode-show-parse-errors nil
-        js2-mode-show-strict-warnings nil
-        js2-skip-preprocessor-directives t
-        js2-strict-missing-semi-warning nil
-        js2-strict-trailing-comma-warning nil)
-  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
+                                                              dap-firefox-debug-path))))
 
 (use-package nodejs-repl
-  :bind (:map js2-mode-map
+  :bind (:map js-ts-mode-map
               ("C-x C-e" . nodejs-repl-send-last-expression)
               ("C-c C-j" . nodejs-repl-send-line)
               ("C-c C-r" . nodejs-repl-send-region)
@@ -44,21 +34,16 @@
               ("C-c C-l" . nodejs-repl-load-file)
               ("C-c C-z" . nodejs-repl-switch-to-repl)))
 
-(use-package rjsx-mode
-  :bind (:map rjsx-mode-map ("C-c C-r" . nodejs-repl-send-region))
-  :mode (("\\.js\\'"  . rjsx-mode)
-         ("\\.jsx\\'" . rjsx-mode)))
-
 (use-package npm-mode
-  :hook ((js2-mode js2-jsx-mode) . npm-mode))
+  :hook ((js-ts-mode tsx-ts-mode) . npm-mode))
 
 (use-package prettier-js
-  :hook ((js2-mode js2-jsx-mode) . prettier-js-mode))
+  :hook ((js-ts-mode tsx-ts-mode) . prettier-js-mode))
 
 (use-package typescript-ts-mode
   :hook ((typescript-ts-mode . lsp))
   :mode (("\\.ts\\'"  . typescript-ts-mode)
-         ("\\.tsx\\'" . typescript-ts-mode)))
+         ("\\.tsx\\'" . tsx-ts-mode)))
 
 (provide 'my-js)
 
