37014f967d78681990f7177c4be8b85f96bb406f
Francisco Soto <ebobby@ebobby.org>
2024-09-16 13:51:19 -0700

    Nice font.

--- core/my-ui.el
+++ core/my-ui.el
@@ -60,7 +60,7 @@
 ;; Frame and font setup for standalone emacs.
 (when window-system
   (my-maximize)
-  (set-frame-font "GitLab Mono-11"))
+  (set-frame-font "CaskaydiaMono Nerd Font-13"))
 
 (provide 'my-ui)
 
