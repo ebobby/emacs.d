7c0c8026c1ed84d8215aec3af2767dc4a867e72d
Francisco Soto <ebobby@ebobby.org>
2024-10-26 13:35:20 -0700

    Font res.

--- core/my-ui.el
+++ core/my-ui.el
@@ -63,9 +63,9 @@
 ;; Frame and font setup for standalone emacs.
 (when window-system
   (my-maximize)
-  (if (eq system-type 'darwin)
-      (set-frame-font "CaskaydiaMono Nerd Font-14")
-    (set-frame-font "CaskaydiaMono Nerd Font-12")))
+  (cond ((eq system-type 'darwin) (set-frame-font "CaskaydiaMono Nerd Font-14"))
+        ((>= (display-pixel-width) 2560) (set-frame-font "Ubuntu Mono NF-14"))
+        (t (set-frame-font "Ubuntu Mono NF-12"))))
 
 (provide 'my-ui)
 
